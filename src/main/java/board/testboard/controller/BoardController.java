9dd5721436c9ae43d18e63798e9a088413b81eaf
한창윤 <hansyooni11@gmail.com>
2023-10-07 21:31:28 +0900

    mapper,controller
    mapper get,set , controller 리팩토링

--- src/main/java/board/testboard/controller/BoardController.java
+++ src/main/java/board/testboard/controller/BoardController.java
@@ -1,10 +1,24 @@
 package board.testboard.controller;
 
+import board.testboard.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping("/submit")
+    public String submitForm(@RequestParam String title, @RequestParam String content) {
+        postService.createPost(title, content);
+        return "redirect:/";
+    }
+
     @GetMapping("/")
     public  String index(){
         return "index";
